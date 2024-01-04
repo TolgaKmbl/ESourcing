d25acf3b4754885ef1f30b4c0f84bba0d9003ad8
TolgaKmbl <tolgakumbul@gmail.com>
2024-01-04 22:27:16 +0300

    Add project files.

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
