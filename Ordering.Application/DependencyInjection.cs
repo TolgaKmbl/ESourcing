96f1d0d784f354ce54fdb2c93e3cd58c0f5cbec3
TolgaKmbl <tolgakumbul@gmail.com>
2024-02-08 21:25:32 +0300

    pipeline behaviours

--- Ordering.Application/DependencyInjection.cs
+++ Ordering.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Mapper;
+using Ordering.Application.PipelineBehaviours;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +23,11 @@ namespace Ordering.Application
              * If using MediatR version < 12, install DependencyInjection package with it and use the code below
              * services.AddMediatR(Assembly.GetExecutingAssembly());
              */
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));       
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
 
             #region Configure Mapper
             var config = new MapperConfiguration(cfg =>
